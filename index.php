5a935d4a60fb68ba118c4e8676f6cce9c7e33b0b
See Guo Lin <see@guol.in>
2011-06-11 18:26:20 +0800

    added aside and content area, and converted into tab indentation

--- index.php
+++ index.php
@@ -9,16 +9,16 @@ $charset = strtolower(get_bloginfo('charset'));
 <!--[if IE 8]>    <html class="no-js ie8 oldie" lang="<?php echo $lang; ?>"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" lang="<?php echo $lang; ?>"> <!--<![endif]-->
 <head>
-  <meta charset="<?php echo $charset; ?>">
+<meta charset="<?php echo $charset; ?>">
 
-  <!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
-       Remove this if you use the .htaccess -->
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
+	Remove this if you use the .htaccess -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-  <title><?php
+<title><?php
 	/*
-	 * Print the <title> tag based on what is being viewed.
-	 */
+	* Print the <title> tag based on what is being viewed.
+	*/
 	global $page, $paged;
 
 	wp_title( '|', true, 'right' );
@@ -35,54 +35,58 @@ $charset = strtolower(get_bloginfo('charset'));
 	if ( $paged >= 2 || $page >= 2 )
 		echo ' | ' . sprintf( 'Page %s', max( $paged, $page ) );
 
-  ?></title>
+?></title>
 
-  <!-- Mobile viewport optimized: j.mp/bplateviewport -->
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  
-  <link rel="shortcut icon" href="<?php bloginfo('stylesheet_directory'); ?>/img/favicon/favicon.ico">
-  <link rel="apple-touch-icon" href="<?php bloginfo('stylesheet_directory'); ?>/img/favicon/apple-touch-icon-precomposed.png">
-  <link rel="apple-touch-icon" sizes="72x72" href="<?php bloginfo('stylesheet_directory'); ?>/img/favicon/apple-touch-icon-72x72-precomposed.png" />
-  <link rel="apple-touch-icon" sizes="114x114" href="<?php bloginfo('stylesheet_directory'); ?>/img/favicon/apple-touch-icon-114x114-precomposed.png" />
+<!-- Mobile viewport optimized: j.mp/bplateviewport -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <!-- Place favicon.ico and apple-touch-icon.png in the root directory: mathiasbynens.be/notes/touch-icons -->
+<link rel="shortcut icon" href="<?php bloginfo('stylesheet_directory'); ?>/img/favicon/favicon.ico">
+<link rel="apple-touch-icon" href="<?php bloginfo('stylesheet_directory'); ?>/img/favicon/apple-touch-icon-precomposed.png">
+<link rel="apple-touch-icon" sizes="72x72" href="<?php bloginfo('stylesheet_directory'); ?>/img/favicon/apple-touch-icon-72x72-precomposed.png" />
+<link rel="apple-touch-icon" sizes="114x114" href="<?php bloginfo('stylesheet_directory'); ?>/img/favicon/apple-touch-icon-114x114-precomposed.png" />
 
-  <!-- CSS: implied media="all" -->
-  <link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>">
-  
-  <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-  <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
+<!-- Place favicon.ico and apple-touch-icon.png in the root directory: mathiasbynens.be/notes/touch-icons -->
 
-  <!-- More ideas for your <head> here: h5bp.com/docs/#head-Tips -->
+<!-- CSS: implied media="all" -->
+<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>">
 
-  <?php wp_head(); ?>
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
+
+<!-- More ideas for your <head> here: h5bp.com/docs/#head-Tips -->
+
+<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 
-  <div class="container">
-    <header>
-      <hgroup>
-        <h1 id="site-title"><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-        <h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
-      </hgroup>
-      <nav id="access" role="navigation">
-        <h1 class="section-heading visuallyhidden">Main menu</h1>
-        <div class="skip-link visuallyhidden"><a href="#main" title="Skip to content">Skip to content</a></div>
-        
-        <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-      </nav><!-- #access -->
-    </header>
-    
-    <div id="main" role="main">
-      
-    </div>
-    
-    <footer>
-      <?php wp_nav_menu( array( 'theme_location' => 'footer' ) ); ?>
-    </footer>
-  </div> <!--! end of .container -->
+<div class="container">
+	<header>
+		<hgroup>
+			<h1 id="site-title"><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
+		</hgroup>
+		<nav id="access" role="navigation">
+			<h1 class="section-heading visuallyhidden">Main menu</h1>
+			<div class="skip-link visuallyhidden"><a href="#content" title="Skip to content">Skip to content</a></div>
+			
+			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+		</nav><!-- #access -->
+	</header>
+	
+	<div id="main" role="main">
+		<div id="content">
+		</div>
+		
+		<aside>
+		</aside>
+	</div>
+	
+	<footer>
+	<?php wp_nav_menu( array( 'theme_location' => 'footer' ) ); ?>
+	</footer>
+</div> <!--! end of .container -->
 
-  <?php wp_footer(); ?>
+<?php wp_footer(); ?>
 </body>
 </html>
