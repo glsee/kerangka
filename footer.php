7e131b42e0c5ba97420dad460303bdddba397975
See Guo Lin <see@guol.in>
2011-06-11 18:40:50 +0800

    introduction of header, footer and sidebar files

---
+++ footer.php
@@ -0,0 +1,10 @@
+	</div><!-- #main -->
+	
+	<footer>
+	<?php wp_nav_menu( array( 'theme_location' => 'footer' ) ); ?>
+	</footer>
+</div> <!--! end of .container -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
